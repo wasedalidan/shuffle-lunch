7e63a678c230ec1861d4007b81d36666b37cd216
Li Dan <dlutlidan@gmail.com>
2014-11-21 10:03:29 +0000

    Posts テーブルの追加

---
+++ app/Config/Migration/1416563583_create_posts.php
@@ -0,0 +1,57 @@
+<?php
+class CreatePosts extends CakeMigration {
+
+/**
+ * Migration description
+ *
+ * @var string
+ */
+	public $description = 'create_posts';
+
+/**
+ * Actions to be performed
+ *
+ * @var array $migration
+ */
+	public $migration = array(
+		'up' => array(
+			'create_table' => array(
+				'posts' => array(
+					'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
+					'title' => array('type' => 'string', 'null' => false, 'default' => null),
+					'body' => array('type' => 'text', 'null' => false, 'default' => null),
+					'create' => array('type' => 'string', 'null' => false, 'default' => null),
+					'modified' => array('type' => 'datetime', 'null' => false, 'default' => null),
+					'indexes' => array(
+						'PRIMARY' => array('column' => 'id', 'unique' => true),
+					),
+				),
+			),
+		),
+		'down' => array(
+			'drop_table' => array(
+				'posts'
+			),
+		),
+	);
+
+/**
+ * Before migration callback
+ *
+ * @param string $direction Direction of migration process (up or down)
+ * @return bool Should process continue
+ */
+	public function before($direction) {
+		return true;
+	}
+
+/**
+ * After migration callback
+ *
+ * @param string $direction Direction of migration process (up or down)
+ * @return bool Should process continue
+ */
+	public function after($direction) {
+		return true;
+	}
+}

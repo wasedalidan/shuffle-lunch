ef9aa6daa2df3a731bdb32e4f61d7ac8dc4e7e1b
Li Dan <dlutlidan@gmail.com>
2014-11-26 09:18:12 +0000

    webapp->root

--- app/Config/bootstrap/environments/development.php
+++ app/Config/bootstrap/environments/development.php
@@ -2,7 +2,7 @@
 
 Environment::configure('development', true, [
 	'MYSQL_DB_HOST' => 'localhost',
-	'MYSQL_USERNAME' => 'webapp',
+	'MYSQL_USERNAME' => 'root',
 	'MYSQL_PASSWORD' => 'password',
 	'MYSQL_DB_NAME' => 'blog',
 	'MYSQL_TEST_DB_NAME' => 'test_blog',

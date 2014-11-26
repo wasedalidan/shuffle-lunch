a3e62a878b7fa22c7a34398e38234a4830b3e6d7
Li Dan <dlutlidan@gmail.com>
2014-11-26 08:03:07 +0000

    summit

--- app/Config/bootstrap/environments/development.php
+++ app/Config/bootstrap/environments/development.php
@@ -2,8 +2,8 @@
 
 Environment::configure('development', true, [
 	'MYSQL_DB_HOST' => 'localhost',
-	'MYSQL_USERNAME' => 'webapp',
-	'MYSQL_PASSWORD' => 'passw0rd',
+	'MYSQL_USERNAME' => 'root',
+	'MYSQL_PASSWORD' => 'password',
 	'MYSQL_DB_NAME' => 'blog',
 	'MYSQL_TEST_DB_NAME' => 'test_blog',
 	'MYSQL_PREFIX' => '',
